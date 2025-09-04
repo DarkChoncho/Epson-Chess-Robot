638465c3f1d6079db76b06883447e2404773f9cb
Tyler Reed <reedtyler27@gmail.com>
2025-09-03 21:53:45 -0700

    Hopefully this works

--- Chess Project/MainWindow.xaml.cs
+++ Chess Project/MainWindow.xaml.cs
@@ -11,8 +11,6 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Automation.Peers;
-using System.Windows.Automation.Provider;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
